a4c15016477ab6839ab08779ae60847de57a21c6
zakaria belamri <83244509+riazakaDev@users.noreply.github.com>
2021-11-20 15:55:10 +0100

    Add files via upload

--- components/navbar.js
+++ components/navbar.js
@@ -4,24 +4,24 @@ import MenuIcon from '@mui/icons-material/Menu';
 const Navbar = () => {
 
   return ( 
-    <div className="div">
-      <nav class="navbar navbar-expand-lg fixed-top ">      
-          <div class="container-fluid container">
+    <div classNameName="div">
+      <nav className="navbar navbar-expand-lg fixed-top ">      
+          <div className="container-fluid container">
           <Link href='/'>
-          <a class="navbar-brand" >Food Make</a>
+          <a className="navbar-brand" >Food Make</a>
           </Link> 
-          <button className="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button classNameName="navbar-toggler " type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <MenuIcon />
           </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav ms-auto">
-                <li class="nav-item">
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
                   <Link href='/blog'>
-                  <a class="nav-link">Blog</a>
+                  <a className="nav-link">Blog</a>
                   </Link>
                 </li>
-                <li class="nav-item">          
-                  <a class="nav-link">About</a>
+                <li className="nav-item">          
+                  <a className="nav-link">About</a>
                 </li>
               </ul>
               
