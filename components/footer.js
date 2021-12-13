341d8d63bb9c2a874a30ba0b5716ce97e3bfe414
zakaria belamri <83244509+riazakaDev@users.noreply.github.com>
2021-12-13 12:05:51 +0100

    Add files via upload

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,26 @@
+
+
 const Footer = () => {
     return ( 
-        <div className="footer d-flex align-items-center justify-content-center" style={{width : "100%"}}>
-            <h3>Developed by Zakaria Belamri</h3>
+        <div className="footer">
+            <div className="container footer-row row">
+                <div className="col-lg-3 footer-col">
+                    <h5>NEWSLETTER</h5>
+                    <input type="text" placeholder='Your e-mail'/>
+                    <p>Sign up for receive special offers!</p>
+                </div>
+                <div className="col-lg-3 footer-col">
+                    <h5>COMPANY</h5>
+                    <p>Shippings & Deilvery</p>
+                    <p>Returns</p>
+                    <p>FAQ</p>
+                    <p>About Us</p>
+                    <p>Careers</p>
+                    <p>Contact Us</p>
+                    <p>Terms</p>
+                    <p>Cookies</p>
+                </div>
+            </div>
         </div>
      );
 }
--- components/swiperh.js
+++ components/swiperh.js
@@ -1,47 +1,12 @@
-import React, { useRef, useState } from "react";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+import bg from '../images/bg.jpg'
+import Image from 'next/dist/client/image';
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination"
-
-
-
-// import Swiper core and required modules
-import SwiperCore, {
-  Pagination
-} from 'swiper';
-
-// install Swiper modules
-SwiperCore.use([Pagination]);
-
-export default function Swiperh() {
-
-    return ( 
-        <Swiper spaceBetween={0} 
-        pagination={{"clickable": true}} 
-        autoplay={{
-            "delay": 3500,
-            "disableOnInteraction": false
-          }}
-        
-        className="mySwiper container-fluid">
-    <SwiperSlide className='bg-1'>
-          <div className='bg-1-1'>
-              <h1>Welcome to Make Food</h1>
-              <h1>Blog</h1>
-              <div className='bg-1-btn'>
-                <button>Explore</button>
-          </div>
-          </div>
-          <div className="blur"></div>
-      <img src="https://images.pexels.com/photos/1040685/pexels-photo-1040685.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className='img-fluid'/>
-      </SwiperSlide>
-    <SwiperSlide><img src='https://images.pexels.com/photos/2403392/pexels-photo-2403392.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940' alt="" className='img-fluid' /></SwiperSlide>
-    <SwiperSlide><img src="https://images.pexels.com/photos/674584/pexels-photo-674584.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className='img-fluid'/></SwiperSlide>
-    <SwiperSlide><img src="https://images.pexels.com/photos/3009323/pexels-photo-3009323.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className='img-fluid'/></SwiperSlide>
-    <SwiperSlide><img src="https://images.pexels.com/photos/724291/pexels-photo-724291.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className='img-fluid'/></SwiperSlide>
-  </Swiper>
-     );
-  }
+const Swiperh = () => {
+  return ( 
+    <div className="bg-home">
+        <Image src={bg} className='img-fluid'/>
+    </div>
+   );
+}
+ 
+export default Swiperh;
